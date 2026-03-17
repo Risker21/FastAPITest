dfc1aa01556c6cd86616f9ba52d8b3740b1de2d9
Risker21 <3225298824@qq.com>
2026-03-17 18:17:04 +0800

    提交

--- Emp/test.py
+++
@@ -1,8 +0,0 @@
-from fastapi import APIRouter
-
-emp = APIRouter(prefix="/emp", tags=["企业员工"])
-
-@emp.get("emp/{id}")
-def get_emp(id:int):
-    print(f'根据{id}查询企业员工')
-    return {'message': 'OK'}
--- main.py
+++ main.py
@@ -1,13 +1,6 @@
 from fastapi import FastAPI
 
-from Emp.test import emp
-
 app = FastAPI()
-
-
-app.include_router(emp)
-
-
 
 
 @app.get("/")
