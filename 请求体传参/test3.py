49f1e3e0b9b8407fccff5a0443da17a38a3ef4b0
Risker21 <3225298824@qq.com>
2026-03-18 11:48:25 +0800

    提交

--- 请求体传参/test3.py
+++ 请求体传参/test3.py
@@ -1,8 +1,9 @@
 from datetime import date
 from enum import Enum
 
-from fastapi import APIRouter
-from pydantic import BaseModel, Field
+from fastapi import APIRouter, Body
+from pydantic import BaseModel, Field, field_validator
+from pygments.lexer import default
 
 test3 = APIRouter(prefix="/test3", tags=["请求体传参测试"])
 
@@ -21,16 +22,35 @@ class Gender(Enum):
     FEMALE = "女"
     OTHER = "未知"
 
-
-
 class Emp(BaseModel):
-    name: str
-    age: int
-    gender: Gender
-    birthday: date
-    address: Addr
+    name: str = Field(description="姓名")
+    age: int = Field(description="年龄",default=18,lt=65,ge=18)
+    gender: Gender = Field(description="性别")
+    birthday: date = Field(description="出生日期")
+    address: Addr = Field(description="地址")
+
+    @field_validator('name')
+    def validate_name(cls,value):
+        pass
+        import re
+        result = re.match(r'^[a-zA-Z_]\w{2,7}$',value)
+        assert result is not None
+        return value
 
 @test3.post("/add", summary="添加员工", description="添加员工信息")
 def find_all(emp: Emp):
     print(emp)
     return {"msg": "OK"}
+
+'''
+请求体传参
+'''
+@test3.post("/test3/select", summary="请求体传参", description="测试请求体传参")
+def test(name:str = Body(default = None,summary="姓名",description="姓名描述"),
+         age:int = Body(default = 18,lt=65,ge=18)):
+    print(f'姓名：{name}，年龄：{age}')
+    return {"msg": "OK"}
+
+
+
+
